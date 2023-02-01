23c7a8ff8e44db5b5b229fe507737908affdfae6
FerdinandoPH <perez.holguin@gmail.com>
2023-02-01 23:41:08 +0100

    mejorillas

--- web5/js/script3.js
+++ web5/js/script3.js
@@ -1,7 +1,19 @@
+var hola
 function Suena(){
     sonido=new Audio("fx/n.mp3");
     sonido.pause();
     sonido.loop=true;
+    try{
     sonido.play();
+    document.getElementById("cuerpo").removeEventListener("click", Suena);
+    }
+    catch(err){
+        console.log("Error: "+err);
+    }
 }
-window.onload=Suena;
+
+window.onload=function(){
+    cuerpo=document.getElementById("cuerpo");
+    hola=cuerpo.addEventListener("click", Suena);
+    Suena();
+}
