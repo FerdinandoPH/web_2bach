26723e5b0f8ad31220e09122304873855da84c94
FerdinandoPH <perez.holguin@gmail.com>
2023-02-06 11:27:45 +0100

    avance del oraculo lunes

--- web5/chat.py
+++ web5/chat.py
@@ -1,5 +1,10 @@
-from revChatGPT import Official
-import websockets,asyncio,base64,openai
+try:
+    from revChatGPT import Official
+    import websockets,asyncio,base64,openai
+except:
+    import os,sys
+    print("Faltan paquetes")
+    sys.exit()
 chat=Official.Chatbot(api_key="")
 async def mandaMensage(websocket, path):
     print("ALGUIEN ")
@@ -7,9 +12,10 @@ async def mandaMensage(websocket, path):
     listapalabras = palabras.split("YYY")
     print(listapalabras)
     print("Creando historia")
-    historia=chat.ask("Crea una breve historia que incluya los siguientes elementos: "+listapalabras[0]+listapalabras[1]+listapalabras[2])["choices"][0]["text"]
+    historia=chat.ask("Crea una breve historia que incluya los siguientes elementos: "+listapalabras[0]+", "+listapalabras[1]+", y "+listapalabras[2])["choices"][0]["text"]
     print(historia)
     await websocket.send("H"+historia)
+    '''
     print("Creando imagen")
     response = openai.Image.create(
     prompt=historia,
@@ -19,6 +25,7 @@ async def mandaMensage(websocket, path):
     image_url = response['data'][0]['url']
     print(image_url)
     await websocket.send("I"+image_url)
+    '''
     await websocket.close()
 async def main():
     print("init")
