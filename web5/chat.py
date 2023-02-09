6166b524db1b895983d9199d3b9b67133f95ebef
FerdinandoPH <perez.holguin@gmail.com>
2023-02-10 00:16:39 +0100

    a ver si ya

--- web5/chat.py
+++ web5/chat.py
@@ -1,5 +1,19 @@
-from revChatGPT import Official
-import websockets,asyncio,base64,openai
+try:
+    from revChatGPT import Official
+    import websockets,asyncio,openai
+except Exception as e:
+    import os,sys
+    print("Error: ",e)
+    '''
+    print("Faltan paquetes")
+    os.system("pip3 install websockets")
+    os.system("pip3 install asyncio")
+    os.system("pip3 install openai")
+    os.system("pip3 install revChatGPT")
+    print("instalados")
+    '''
+    sys.exit()
+    
 chat=Official.Chatbot(api_key="")
 async def mandaMensage(websocket, path):
     print("ALGUIEN ")
@@ -7,9 +21,10 @@ async def mandaMensage(websocket, path):
     listapalabras = palabras.split("YYY")
     print(listapalabras)
     print("Creando historia")
-    historia=chat.ask("Crea una breve historia que incluya los siguientes elementos: "+listapalabras[0]+listapalabras[1]+listapalabras[2])["choices"][0]["text"]
+    historia=chat.ask("Crea una breve historia que incluya los siguientes elementos: "+listapalabras[0]+", "+listapalabras[1]+", y "+listapalabras[2],temperature=0.6)["choices"][0]["text"]
     print(historia)
     await websocket.send("H"+historia)
+    '''
     print("Creando imagen")
     response = openai.Image.create(
     prompt=historia,
@@ -19,10 +34,11 @@ async def mandaMensage(websocket, path):
     image_url = response['data'][0]['url']
     print(image_url)
     await websocket.send("I"+image_url)
+    '''
     await websocket.close()
 async def main():
     print("init")
-    async with websockets.serve(mandaMensage, "localhost", 3333):
+    async with websockets.serve(mandaMensage, "localhost", 44445): #type: ignore
         await asyncio.Future()
 asyncio.run(main())
 #'''
--- web5/js/scriptOraculo.js
+++ web5/js/scriptOraculo.js
@@ -1,12 +1,25 @@
-let socket= new WebSocket("ws://localhost:3333");
-socket.onopen=function(event){
-socket.send("regaloYYYlamparaYYYgenioYYYprueba");
+function ContinuaIntro (p){
+    if(p==1){
+        //hola
+    }
 }
-socket.onmessage=function(event){
-    if (event.data[0]=="H")
-        console.log(event.data.substring(1));
-    else if (event.data[0]=="I"){
-        var link=event.data.substring(1);
-        document.getElementById("imagenhistoria").src=link;
+function Generar(){
+    var mensaje=document.getElementById("palabra1").value+"YYY"+document.getElementById("palabra2").value+"YYY"+document.getElementById("palabra3").value;
+    console.log(mensaje);
+    let socket= new WebSocket("ws://localhost:3333");
+    socket.onopen=function(event){
+        socket.send(mensaje);
+        document.body.style.backgroundImage="url('img/procesando.gif')";
+    }
+    socket.onmessage=function(event){
+        document.body.style.backgroundImage="url('img/oraculoTemplo.jpg')";
+        if (event.data[0]=="H"){
+            console.log(event.data.substring(1));
+            document.getElementById("resultado").innerHTML=event.data.substring(1);
+        }
+        else if (event.data[0]=="I"){
+            var link=event.data.substring(1);
+            document.getElementById("imagenhistoria").src=link;
+        }
     }
 }
