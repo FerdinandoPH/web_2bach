33fb65884dc994a2df286dc2620adacbe4d6b909
FerdinandoPH <perez.holguin@gmail.com>
2023-02-11 17:54:45 +0100

    backend

--- web5/backend.py
+++ web5/backend.py
@@ -7,7 +7,7 @@ async def mandaMensage(websocket, path):
     listapalabras = palabras.split("YYY")
     print(listapalabras)
     print("Creando historia")
-    historia=chat.ask("Crea una breve historia en la que el protagonista, llamado ")["choices"][0]["text"]
+    historia=chat.ask("Crea una breve historia en la que el (o la) protagonista sea un campesino/a del antiguo egipto llamado "+listapalabras[0]+". La historia debe ocurrir en "+listapalabras[3]+". En algún punto de la historia debe aparecer "+listapalabras[5]+", "+listapalabras[2]+", y el número "+listapalabras[1]+". Además, en algún momento de la historia el protagonista debe "+listapalabras[4]+". La historia debe acabar con el protagonista siendo nombrado faraón",temperature=0.7)["choices"][0]["text"]
     print(historia)
     await websocket.send("H"+historia)
     '''
@@ -27,6 +27,3 @@ async def main():
     async with websockets.serve(mandaMensage, "localhost", 44445): #type: ignore
         await asyncio.Future()
 asyncio.run(main())
-#'''
-
-#'''
