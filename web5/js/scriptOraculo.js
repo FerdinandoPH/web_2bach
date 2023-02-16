b50c5d77dff9aa406f5f3539428f3874b50faa50
FerdinandoPH <perez.holguin@gmail.com>
2023-02-16 19:14:49 +0100

    mejora del tamaño

--- web5/js/scriptOraculo.js
+++ web5/js/scriptOraculo.js
@@ -122,8 +122,16 @@ function ColocaPictografia(urls) {
     var listaurls=urls.split("YYY");
     var textoHistoria=document.getElementById("resultado").innerHTML;
     for (i=0;i<sustantivos.length;i++){
-        var patron= new RegExp(sustantivos[i].replace('.',''), 'gi');
-        textoHistoria=textoHistoria.replace(patron,sustantivos[i]+"<img src=\""+listaurls[i]+"\" class=\"picto\"/>");
+        var patron= new RegExp(sustantivos[i].replace('.',''), 'i');
+        textoHistoria=textoHistoria.replace(patron,sustantivos[i]+"<img src=\""+listaurls[i]+"\" class=\"picto\" id=\"picto"+i+"\"/>");
+        if (document.getElementById("picto"+i).naturalWidth>document.getElementById("picto"+i).naturalHeight){
+            document.getElementById("picto"+i).style.width="100px";
+            document.getElementById("picto"+i).style.height="auto";
+        }
+        else{
+            document.getElementById("picto"+i).style.width="auto";
+            document.getElementById("picto"+i).style.height="100px";
+        }
     }
     document.getElementById("resultado").innerHTML=textoHistoria;
 
