38d7868c34601187a2e74e34ccb5ebfc47303a11
FerdinandoPH <perez.holguin@gmail.com>
2023-02-17 10:22:17 +0100

    bugfix

--- web5/js/scriptOraculo.js
+++ web5/js/scriptOraculo.js
@@ -122,8 +122,7 @@ function ColocaPictografia(urls) {
     var listaurls=urls.split("YYY");
     var textoHistoria=document.getElementById("resultado").innerHTML;
     for (i=0;i<sustantivos.length;i++){
-        var patron= new RegExp(sustantivos[i].replace('.',''), 'i');
-        textoHistoria=textoHistoria.replace(patron,sustantivos[i]+"<img src=\""+listaurls[i]+"\" class=\"picto\" id=\"picto"+i+"\"/>");
+        textoHistoria=textoHistoria.replace(sustantivos[i],sustantivos[i]+"<img src=\""+listaurls[i]+"\" class=\"picto\" id=\"picto"+i+"\"/>");
     }
     document.getElementById("resultado").innerHTML=textoHistoria;
 
