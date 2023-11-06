4edc2e403690a7e325a5ac6a62321c31185745d1
FerdinandoPH <perez.holguin@gmail.com>
2023-11-06 09:27:27 +0100

    arreglos finales

--- web5/backend.py
+++ web5/backend.py
@@ -8,13 +8,13 @@ A partir del 20/02/2023, voy a apagar mi servidor, asi que para que funcione el 
         para la conexión a internet
     b: openai (pip install openai)
         Se encarga de crear la historia y de contactar con DALL-E para generar la imagen del final. Es necesario crear una cuenta en https://openai.com/ para obtener la llave de la API en https://platform.openai.com/account/api-keys . 
-        ...Una vez creada, ponla en el hueco marcado en el código (línea 118).
+        ...Una vez creada, ponla en el hueco marcado en el código (línea 121).
         ADVERTENCIA: Dall-E es de pago (1,6 céntimos por imagen), así que he comentado el código que lo utiliza
     c: spacy (pip install spacy)
         Se encarga de analizar la historia para encontrar los 5 sustantivos aleatorios de los que se harán los pictogramas
     d: serpapi (pip install google-search-results)
         Se encarga de buscar en google las imagenes para los pictogramas.
-        Es necesario crear una cuenta en https://serpapi.com/ para obtener la llave de la API. Una vez creada, ponla en el hueco marcado en el código (línea 89). Se pueden hacer 100 peticiones al mes de forma gratuita.
+        Es necesario crear una cuenta en https://serpapi.com/ para obtener la llave de la API. Una vez creada, ponla en el hueco marcado en el código (línea 91). Se pueden hacer 100 peticiones al mes de forma gratuita.
 
 
 Ojo, cuidado con que no se filtren las cuentas/claves API
@@ -36,8 +36,13 @@ try:
     nlp = spacy.load("es_core_news_md")
 except:
     print("No se ha encontrado el modelo de IA de spacy (analizador morfológico de texto). Descargando...")
-    os.system("python -m spacy download es_core_news_md")
-    nlp = spacy.load("es_core_news_md")
+    try:
+        os.system("python -m spacy download es_core_news_md")
+        nlp = spacy.load("es_core_news_md")
+    except:
+        import sys
+        print("No se ha podido descargar el modelo de IA de spacy. Asegúrate de que tienes python añadido al PATH")
+        sys.exit(1)
 async def MandaMensaje(websocket, path):
     print("ALGUIEN SE HA CONECTADO")
     palabras = await websocket.recv()
@@ -45,7 +50,6 @@ async def MandaMensaje(websocket, path):
     print(listapalabras)
     print("Creando historia")
     historia=""
-    sust=""
     prompthistoria=""
     promptimagen=""
     if listapalabras[6]=="hombre":
@@ -55,7 +59,6 @@ async def MandaMensaje(websocket, path):
         prompthistoria="Crea una breve historia en la que la protagonista sea una campesina del antiguo Egipto, llamada "+listapalabras[0]+" y de carácter "+listapalabras[5]+", que ha vuelto a su casa después de que un oráculo haya predicho que sería la futura faraona. La historia debe ocurrir en "+listapalabras[3]+". En algún punto de la historia debe aparecer el siguiente animal: "+listapalabras[2]+", y el número "+listapalabras[1]+". Además, en algún momento de la historia la protagonista debe "+listapalabras[4]+". La historia debe acabar con la protagonista siendo nombrada faraona. Ten en cuenta que es muy inusual que una campesina se convierta en faraona, así que para que la historia tenga sentido, el antiguo faraón tiene que retirarse (debido a su muerte y la falta de descendientes, una revolución, su abdicación, u otra circunstancia) y debe ocurrir algo extraordinario para que la protagonista sea elegida como nueva faraona."
         promptimagen="Una fotografía a color de una campesina del antiguo Egipto con la corona de faraona en su cabeza, en unos campos en"+listapalabras[3]+", y con "+listapalabras[2]+" a su lado."
     try:
-            
         cuentacuentos=openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -78,7 +81,7 @@ async def MandaMensaje(websocket, path):
         print(lista_sustantivos)
         await websocket.send("S"+",".join(lista_sustantivos))
         print("Creando pictogramas")
-        urlssust=[]
+        urls_sustantivos=[]
         for sustantivo in lista_sustantivos:
             params = {
                 "q": sustantivo,
@@ -90,13 +93,13 @@ async def MandaMensaje(websocket, path):
             search = GoogleSearch(params)
             results = search.get_dict()
             images_results = results["images_results"][0]["original"]
-            urlssust.append(images_results)
-        print(urlssust)
-        stringsust=""
+            urls_sustantivos.append(images_results)
+        print(urls_sustantivos)
+        string_sustantivos=""
         separador="YYYTAMBIEN"
-        for url in urlssust:
-            stringsust+=url+separador
-        await websocket.send("U"+stringsust)
+        for url in urls_sustantivos:
+            string_sustantivos+=url+separador
+        await websocket.send("U"+string_sustantivos)
         #Sección comentada porque es de pago
         # print("Creando imagen")
         # response = openai.Image.create(
@@ -112,10 +115,11 @@ async def MandaMensaje(websocket, path):
         print("Error: "+str(e))
         print("traceback: "+traceback.format_exc())
         await websocket.send("E")
+        await websocket.close()
     await websocket.close()
 async def main():
     openai.api_key=os.getenv("OPENAI_API_KEY") #PON TU CLAVE API DE OPENAI AQUÍ
-    print("init")
+    print("Servidor iniciado")
     async with websockets.serve(MandaMensaje, "localhost", 3333): #type: ignore
         await asyncio.Future()
 asyncio.run(main())
